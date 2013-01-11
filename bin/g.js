d10a8d01e162b0d2d422b1a48a2462b16c9dd5e8
John Fawcett <jrf0110@gmail.com>
2013-01-10 23:30:24 -0600

    updated readme added bin file

--- bin/g.js
+++ bin/g.js
@@ -1,6 +1,6 @@
 var
   goog = require('./')
-, input = process.argv.length > 2 ? process.argv.slice(2).join(' ') : 'feynman'
+, input = process.argv.length > 2 ? process.argv.slice(2).join(' ') : 'npm googenterm'
 ;
 
 goog.query(input, function(error){
